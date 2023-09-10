c3c81925c72c7a43107d000bc453832a6eb7cfc7
DaoQuocHuy <75303365+DaoQuocHuy@users.noreply.github.com>
2023-09-10 23:10:40 +0700

    seesion4

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql2');
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
 const app = express();
@@ -10,7 +11,25 @@ const port = process.env.PORT || 3000;
 configViewEngine(app);
 
 // khai bao router
-app.use(webRoutes);
+app.use('/',webRoutes);
+
+// test connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3307,
+  user: 'root',
+  password: '123456',
+  database: 'quochuy'
+});
+
+// query 
+connection.execute(
+  'SELECT * FROM Users u ',
+  ['Rick C-137', 53],
+  function(err, results, fields) {
+    console.log(results); // results contains rows returned by server
+  }
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
